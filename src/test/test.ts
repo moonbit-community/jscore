263e6652361c6096cd3583e841484681459ff0c6
猗露 <69190413+illusory0x0@users.noreply.github.com>
2025-03-21 09:22:46 +0800

    test: add catch panic

--- src/test/test.ts
+++ src/test/test.ts
@@ -5,9 +5,11 @@ let wasmUrl = './target/wasm-gc/release/build/test/test.wasm'
 const wasmBuffer = await fs.readFile(wasmUrl);
 const wasmModule = await WebAssembly.instantiate(wasmBuffer, importObject);
 let { entry_test,
+  catch_panic
 } = wasmModule.instance.exports as {
-  entry_test: arrow<Unit, Unit>
+  entry_test: arrow<Unit, Unit>,
+  catch_panic: arrow<Unit, Unit>
 }
 
-
+catch_panic()
 entry_test()
